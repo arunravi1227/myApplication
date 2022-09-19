72067abd06ff552c2601f43e5b9f06c4cefc8dd8
arunravi1227 <arunravi1227@gmail.com>
2022-09-19 16:07:34 +0530

    add if for block

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  title:any="enter your name"
-  isdisabled:boolean=false;
+  title:any="Enter your name";
+
+  isdisabled:boolean=false; 
+  isActive:boolean=true;
+  students = [{studentId:1,studentName:'Arun'},{studentId:2,studentName:'Vicky'},{studentId:3,studentName:'Vijay'}]
 
 
   constructor() { }
