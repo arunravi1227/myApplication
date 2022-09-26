8e130bc6e6cb3cec27be9fe3d98f1935ede3459b
arunravi1227 <arunravi1227@gmail.com>
2022-09-26 07:48:08 +0530

    navigation

--- src/app/aboutus/aboutus.component.ts
+++ src/app/aboutus/aboutus.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-aboutus',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutusComponent implements OnInit {
 
-  constructor() { }
+  constructor(private compo:Router,private compo2:ActivatedRoute ) { }
+  
 
   ngOnInit(): void {
+  }
+  navigateToComponoent1(){
+    this.compo.navigate(['lists'],{relativeTo:this.compo2})
+
   }
 
 }
